bb8c5fd43d28baf1d2c9e875821dc102f4ff77fc
Acl <SDJurchenko@stud.kantiana.ru>
2022-08-25 16:06:33 +0300

    добавленные новые команды

--- tg_bot.py
+++ tg_bot.py
@@ -1,16 +1,13 @@
 #Импорт библиотек и констант
 import telebot
-import mysql.connector
 from bo_const import *
-from mysql.connector import errorcode
-
-#константы
-# token = '5744505847:AAEaSl_GI5r5mc4YUgEYSYCSgcyQzjAolZk'
-# bot = telebot.TeleBot(token)
-# types = telebot.types
-
 
 #основная часть кода
+
+@bot.callback_query_handler(lambda call: True)
+def callback_handler(call):
+    if call.data == 'pressed_call':
+        bot.send_message(call.from_user.id,'@Aclyssgay')
 
 @bot.message_handler(commands=['start'])
 def dialog_start(message):
@@ -19,30 +16,52 @@ def dialog_start(message):
     contact_button = types.KeyboardButton('Контакты')
     curs_button = types.KeyboardButton('Курсы')
     markup.add(info_button, contact_button,curs_button)
-    bot.send_message(message.chat.id,'Hello World! Мы являемся онлайн-центом «NoName», цель которого является повышение '
+    bot.send_message(message.chat.id,'Hello World! Мы являемся онлайн-центом «ProfiBoard», цель которого является повышение '
                                      'квалификации участников среднего бизнеса. Мы проводим курсы повышения квалификации '
                                      'с присвоением соответствующих сертификатов.Выберите что-то из предложенных команд.',reply_markup=markup)
+
+@bot.message_handler(commands=['contacts'])
+def button(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button_call = telebot.types.InlineKeyboardButton('Контакты',callback_data='pressed_call')
+    keyboard.add(button_call)
+    bot.send_message(message.chat.id, 'По вопросам обращасться', reply_markup=keyboard)
+
+@bot.message_handler(commands=['curses'])
+def button(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton('URL', url='ProfiBoard.com')
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, 'Наши курсы', reply_markup=keyboard)
+
+@bot.message_handler(commands=['registration'])
+def button(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton('URL', url='https://profidesk.ichernov.ru/')
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, 'Здесь вы можете ознакомится с нашим сайтом, почитать дайджесты и про нас',reply_markup=keyboard)
+
 
 @bot.message_handler(content_types=['text'])
 def text_message(message):
     if message.text == 'Регистрация':
         keyboard = types.InlineKeyboardMarkup()
-        url_button = types.InlineKeyboardButton('URL', url='ProfiBoard.com')
+        url_button = types.InlineKeyboardButton('URL', url='https://profidesk.ichernov.ru/')
         keyboard.add(url_button)
-        bot.send_message(message.chat.id, 'Здесь вы можете зарегистрироваться на наши курсы', reply_markup=keyboard)
-    if message.text =='Контакты':
-        keyboard = types.InlineKeyboardMarkup()
-        message_button = types.InlineKeyboardButton('@Aclyssgay')
-        keyboard.add(message_button)
-        bot.send_message(message.chat.id,'Мы в ТГ @Aclyssgay')
+        bot.send_message(message.chat.id, 'Здесь вы можете ознакомится с нашим сайтом, почитать дайджесты и про нас', reply_markup=keyboard)
+    elif message.text =='Контакты':
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        button_call = telebot.types.InlineKeyboardButton('Контакты', callback_data='pressed_call')
+        keyboard.add(button_call)
+        bot.send_message(message.chat.id, 'По вопросам обращасться', reply_markup=keyboard)
     elif message.text == 'Курсы':
         # Создаем панельку под сообщением
         keyboard = types.InlineKeyboardMarkup()
         url_button = types.InlineKeyboardButton('URL', url='ProfiBoard.com')
-        test_button = types.KeyboardButton('Назад')
-        message_button = types.InlineKeyboardButton('',callback_data='message')  # лучше callback вносить в переменную, чтобы было удобно обрабатывать
-        keyboard.add(url_button, message_button)
+        keyboard.add(url_button)
         bot.send_message(message.chat.id, 'Наши курсы', reply_markup=keyboard)
+
+
 
 
 #активация бота
